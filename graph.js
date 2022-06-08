81b6d5981593cb2f59f043469a83f7ef942c27ff
Vincent Van Buskirk <vvanbuskirk805@gmail.com>
2022-06-08 00:29:36 -0400

    init commit

---
+++ graph.js
@@ -0,0 +1,221 @@
+import preppedData from "./preppedData_new.json" assert { type: "json" };
+import data from "./data_new.json" assert { type: "json" };
+import colorMap from "./colorMap.json" assert { type: "json" };
+
+(function () {
+  const MATCH_WIDTH = 10;
+  const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+  const height = 600;
+  const width = 1200;
+
+  const maxMatchesWeek = d3.max(Object.values(_.groupBy(data, "week")).map(d => d.length));
+
+  const svg = d3.select("#chart").append("svg").attr("viewBox", [0, 0, width, height]);
+
+  const margin = {
+    top: 35,
+    bottom: 75,
+    left: 50,
+    right: 50,
+  };
+
+  const plotWidth = width - margin.left - margin.right;
+  const plotHeight = height - margin.top - margin.bottom;
+
+  const g = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`).attr("class", "plotGroup");
+
+  // Scales
+  const xScale = d3.scaleBand().domain(weeks).range([0, plotWidth]);
+  const yScale = d3.scaleBand().domain(d3.range(maxMatchesWeek)).range([0, plotHeight]).paddingInner(0.3);
+  const gameScale = d3.scaleLinear().domain([0, 100]).range([yScale.bandwidth(), 0]);
+
+  // Week Labels
+  const weekGroups = g
+    .selectAll(".week")
+    .data(preppedData)
+    .join("g")
+    .attr("transform", (d, i) => `translate(${xScale(i + 1) + xScale.bandwidth() / 2}, 0)`);
+
+  const weekLabelGroups = weekGroups
+    .selectAll(".weekLabelGroup")
+    .data((d, i) => [i + 1])
+    .join("text")
+    .attr("x", 0)
+    .attr("y", -35)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "10px")
+    .attr("font-weight", "500")
+    .attr("class", "weekLabelGroup");
+
+  weekLabelGroups
+    .selectAll(".weekLabel")
+    .data(d => ["WEEK", d])
+    .join("tspan")
+    .attr("x", 0)
+    .attr("dy", "0.7rem")
+    .attr("class", "weekLabel")
+    .text(d => d);
+
+  const games = weekGroups
+    .selectAll(".game")
+    .data(d => d)
+    .join("g")
+    .attr("transform", (d, i) => `translate(0, ${yScale(i)})`);
+
+  games
+    .selectAll(".teamLabel")
+    .data(d => d)
+    .join("text")
+    .attr("x", -25)
+    .attr("y", (d, i) =>
+      i % 2 === 0
+        ? gameScale(100) + gameScale(100 - d.success) / 2
+        : gameScale(d.success) + gameScale(100 - d.success) / 2
+    )
+    .attr("text-anchor", "left")
+    .attr("font-size", "8px")
+    .attr("dy", "4")
+    .attr("class", "teamLabel")
+    .attr("fill", d => colorMap[d.team].primary)
+    .text((d, i) => `${d.team}`);
+
+  games
+    .selectAll(".winPercentageLabel")
+    .data(d => d)
+    .join("text")
+    .attr("x", 10)
+    .attr("y", (d, i) =>
+      i % 2 === 0
+        ? gameScale(100) + gameScale(100 - d.success) / 2
+        : gameScale(d.success) + gameScale(100 - d.success) / 2
+    )
+    .attr("text-anchor", "right")
+    .attr("font-size", "8px")
+    .attr("dy", "4")
+    .attr("class", "winPercentageLabel")
+    .attr("opacity", 0)
+    .attr("fill", d => colorMap[d.team].primary)
+    .text((d, i) => `${d.rawSuccess}`);
+
+  games
+    .selectAll(".team")
+    .data(d => d)
+    .join("rect")
+    .attr("x", -MATCH_WIDTH / 2)
+    .attr("y", (d, i) => (i % 2 === 0 ? gameScale(100) : gameScale(d.success)))
+    .attr("height", d => gameScale(100 - d.success))
+    .attr("width", MATCH_WIDTH)
+    .attr("fill", d => colorMap[d.team].primary)
+    .attr("fill-opacity", 1)
+    .attr("stroke", "white")
+    .on("mouseover", (event, data) => {
+      highlightPath(data, gameScale, xScale, yScale);
+      setTooltip(event, data)
+    })
+    .on("mouseout", (event, data) => {
+      g.selectAll(".highlight").data([]).join("path").attr("class", "highlight");
+      g.selectAll(".team").attr("opacity", 1);
+      g.selectAll(".teamLabel").attr("opacity", 1).attr("font-weight", "normal");
+      g.selectAll(".winPercentageLabel").attr("opacity", 0);
+
+      tooltip.style("visibility", "hidden");
+    })
+    .attr("class", "team");
+
+  const areaGame = d3
+    .area()
+    .x(d => d.x)
+    .y0(d => d.y0)
+    .y1(d => d.y1)
+    .curve(d3.curveMonotoneX);
+
+  const buildHighlight = (team, gameScale, xScale, yScale) => {
+    const teamData = data.filter(match => match.teamANickname === team || match.teamBNickname === team);
+    return teamData
+      .map(d => {
+        const teamAccessor = d.teamANickname === team ? "teamASuccess" : "teamBSuccess";
+        const isTopTeam = teamAccessor === "teamASuccess";
+        return [
+          {
+            x: xScale(d.week) + xScale.bandwidth() / 2 - MATCH_WIDTH / 2,
+            y0: yScale(d.game) + (isTopTeam ? gameScale(100) : gameScale(d[teamAccessor])),
+            y1: yScale(d.game) + (isTopTeam ? gameScale(100 - d[teamAccessor]) : gameScale(0)),
+          },
+          {
+            x: xScale(d.week) + xScale.bandwidth() / 2 + MATCH_WIDTH / 2,
+            y0: yScale(d.game) + (isTopTeam ? gameScale(100) : gameScale(d[teamAccessor])),
+            y1: yScale(d.game) + (isTopTeam ? gameScale(100 - d[teamAccessor]) : gameScale(0)),
+          },
+        ];
+      })
+      .flat();
+  };
+
+  const highlightPath = (selectedData, gameScale, xScale, yScale) => {
+    const { team, oppTeam, idx } = selectedData;
+    const g = d3.select(".plotGroup");
+    const highlightData = buildHighlight(team, gameScale, xScale, yScale);
+    const games = data.filter(d => d.teamANickname === team || d.teamBNickname === team).map(d => d.idx);
+
+    g.selectAll(".highlight")
+      .data([highlightData])
+      .join("path")
+      .attr("class", "highlight")
+      .attr("d", d => areaGame(d))
+      .attr("fill", d => colorMap[team].primary)
+      .attr("fill-opacity", 0.3)
+      .lower();
+    g.selectAll(".team").attr("opacity", d => (games.includes(d.idx) ? 1 : 0));
+    g.selectAll(".teamLabel")
+      .attr("opacity", d => (games.includes(d.idx) ? 1 : 0.1))
+      .attr("font-weight", d => (games.includes(d.idx) ? "600" : "normal"));
+    g.selectAll(".winPercentageLabel")
+      .attr("opacity", d => (games.includes(d.idx) ? 1 : 0))
+      .attr("font-weight", d => (games.includes(d.idx) ? "600" : "normal"));
+  };
+
+//   const tooltip = d3.select("#chart").append("div")	
+//   .style("position", "absolute")
+//   .style("width", "200px")
+//   .style("height", "200px")
+//   .style("pointer-events", "none")
+//   .style("opacity", 0);
+
+const tooltip = d3.select("#chart")
+.append("div")
+  .style("position", "absolute")
+  .style("visibility", "hidden")
+  .style("width", "200px")
+  .style("height", "200px")
+  .style("pointer-events", "none")
+
+  const setTooltip = (event, data) => {
+    const { team, oppTeam, rawSuccess, rawOppTeamSuccess} = data;
+    const logoBaseURL = team => `https://sportsbook.draftkings.com/static/logos/teams/nfl/${team}.png`;
+    const teamLinkImage = `<image xlink:href=${logoBaseURL(team)} width="25" height="25"/>`;
+    const oppTeamLinkImage = `<image xlink:href=${logoBaseURL(oppTeam)} width="25" height="25"/>`;
+
+    tooltip
+      .style("top", event.pageY + 20 + "px")
+      .style("left", event.pageX - 65 + "px")
+      .style("visibility", "visible").html(`
+            <div class="info-column info-stat" style="color: ${colorMap[team].primary}">
+              <p class="text-lg">${team}</p>
+              <svg width="25" height="25">       
+                ${teamLinkImage}
+              </svg>
+              <p class="text-lg">${rawSuccess}</p>
+            </div>
+            <div class="info-column">
+              <p class="text-sm">VS</p>
+            </div>
+            <div class="info-column info-stat" style="color: ${colorMap[oppTeam].primary}">
+              <p class="text-lg">${oppTeam}</p>
+              <svg width="25" height="25">       
+                ${oppTeamLinkImage}  
+              </svg>
+              <p class="text-lg">${rawOppTeamSuccess}</p>
+            </div>
+          `);
+  };
+})();
---
+++ scrape.py
@@ -0,0 +1,167 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import re
+import json
+import numpy as np
+
+TEAMS = {
+    'Arizona Cardinals': 'ARI',
+    'Atlanta Falcons': 'ATL',
+    'Baltimore Ravens': 'BAL',
+    'Buffalo Bills': 'BUF',
+    'Carolina Panthers': 'CAR',
+    'Chicago Bears': 'CHI',
+    'Cincinnati Bengals': 'CIN',
+    'Cleveland Browns': 'CLE',
+    'Dallas Cowboys': 'DAL',
+    'Denver Broncos': 'DEN',
+    'Detroit Lions': 'DET',
+    'Green Bay Packers': 'GB',
+    'Houston Texans': 'HOU',
+    'Indianapolis Colts': 'IND',
+    'Jacksonville Jaguars': 'JAX',
+    'Kansas City Chiefs': 'KC',
+    'Los Angeles Chargers': 'LAC',
+    'Los Angeles Rams': 'LAR',
+    'Las Vegas Raiders': 'LV',
+    'Miami Dolphins': 'MIA',
+    'Minnesota Vikings': 'MIN',
+    'New England Patriots': 'NE',
+    'New Orleans Saints': 'NO',
+    'New York Giants': 'NYG',
+    'New York Jets': 'NYJ',
+    'Philadelphia Eagles': 'PHI',
+    'Pittsburgh Steelers': 'PIT',
+    'San Francisco 49ers': 'SF',
+    'Seattle Seahawks': 'SEA',
+    'Tampa Bay Buccaneers': 'TB',
+    'Tennessee Titans': 'TEN',
+    'Washington Football Team': 'WAS'
+}
+
+def normalization(data):
+    norm = [float(i)/sum(data) for i in data]
+    return [round(round(norm[0], 2) * 100, 2), round(round(norm[1], 2) * 100, 2)]
+
+
+def add_title(num):
+    if num == 1 or num == 31:
+        return str(num) + 'ST'
+    elif num == 2:
+        return str(num) + 'ND'
+    elif num == 3:
+        return str(num) + 'RD'
+    elif num >= 4 and num <= 20:
+        return str(num) + 'TH'
+    elif num == 21:
+        return str(num) + 'ST'
+    elif num == 22:
+        return str(num) + 'ND'
+    elif num == 23:
+        return str(num) + 'RD'
+    elif num >= 24 and num <= 30:
+        return str(num) + 'TH'
+
+page = requests.get('https://www.pro-football-reference.com/years/2021/games.htm')
+soup = BeautifulSoup(page.content, 'html.parser')
+
+rows = soup.find_all('tr', class_=lambda x: x != 'thead')
+
+all_dics = []
+all_prepped_dics = []
+current_week_arr = []
+current_week = 1
+current_game = 0
+
+for idx, row in enumerate(rows[1:]):
+    data = row.find_all('td')
+    
+    winner = next(x for x in data if x.get('data-stat') == 'winner')
+    if winner.text:
+        winner = TEAMS[winner.text]
+    else:
+        break
+
+    week = int(row.find('th').text)
+
+
+    if current_week != week:
+        all_prepped_dics.append(current_week_arr)
+        current_week_arr = []
+
+        current_week = week
+        current_game = 0
+
+    loser = TEAMS[next(x for x in data if x.get('data-stat') == 'loser').text]
+    winner_points = int(next(x for x in data if x.get('data-stat') == 'pts_win').text)
+    loser_points = int(next(x for x in data if x.get('data-stat') == 'pts_lose').text)
+    normalized = normalization([int(winner_points), int(loser_points)])
+
+    winner_points_normalized = normalized[0]
+    loser_points_normalized = 100 - winner_points_normalized
+    
+    day = next(x for x in data if x.get('data-stat') == 'game_day_of_week').text.upper()
+
+    game_date = next(x for x in data if x.get('data-stat') == 'game_date').text
+    parsed_date = datetime.datetime.strptime(game_date, "%Y-%m-%d")
+    month = parsed_date.strftime("%b").upper()
+    monthDay = add_title(parsed_date.day)
+
+    event_parsed = re.split('(AM|PM)', next(x for x in data if x.get('data-stat') == 'gametime').text)
+    event = f"{event_parsed[0]} {event_parsed[1]}"
+    
+    all_dics.append({
+        "week": week,
+        "teamANickname": winner,
+        "teamBNickname": loser,
+        "teamASuccess": winner_points_normalized,
+        "teamBSuccess": loser_points_normalized,
+        "day": day,
+        "month": month,
+        "monthDay": monthDay,
+        "event": event,
+        "idx": idx,
+        "game": current_game
+    })
+
+    current_week_arr.append([
+        {
+            "team": winner,
+            "success": winner_points_normalized,
+            "rawSuccess": winner_points,
+            "oppTeam": loser,
+            "oppTeamSuccess": loser_points_normalized,
+            "rawOppTeamSuccess": loser_points,
+            "week": week,
+            "idx": idx
+        },
+        {
+            "team": loser,
+            "success": loser_points_normalized,
+            "rawSuccess": loser_points,
+            "oppTeam": winner,
+            "oppTeamSuccess": winner_points_normalized,
+            "rawOppTeamSuccess": winner_points,
+            "week": week,
+            "idx": idx
+        }
+    ])
+
+    current_game += 1
+
+    if idx == 271:
+        all_prepped_dics.append(current_week_arr)
+
+
+with open("data_new.json", "w") as file:
+    json.dump(all_dics, file, indent=2)
+
+with open("preppedData_new.json", "w") as file:
+    json.dump(all_prepped_dics, file, indent=2)
+
+    
+
+
+
+    
